fe27cc31b5d735d2fb4598cf60b8afc55d515cfd
ArnabSaga <arnabdey15091@gmail.com>
2026-03-02 17:53:12 +0600

    Adding Not Found Page , Error Page, Loading Pages

---
+++ src/app/(dashboradLayout)/(patientRouteGroup)/(patientdashboardLayout)/dashboard/my-prescriptions/loading.tsx
@@ -0,0 +1,4 @@
+export default function PatientsMyPrescriptionsLoading() {
+  // Or a custom loading skeleton component
+  return <p>Patients My Prescriptions Loading...</p>;
+}
---
+++ src/app/(dashboradLayout)/doctor/dashboard/prescriptions/loading.tsx
@@ -0,0 +1,4 @@
+export default function DoctorsPrescriptionsLoading() {
+  // Or a custom loading skeleton component
+  return <p>Doctors Prescriptions Loading...</p>;
+}
